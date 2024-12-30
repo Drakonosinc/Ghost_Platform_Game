c9f14e73abff1cbffe797d348f9d378f30760457
matias <mat20can@gmail.com>
2024-12-29 21:38:57 -0300

    version 9.6

--- Player.py
+++ Player.py
@@ -1,3 +1,25 @@
-class player():
-    def __init__(self):
-        pass
+from pygame import Rect
+class Player:
+    def __init__(self, x, y, width, height):
+        self.rect = Rect(x, y, width, height)
+        self.down_gravity = 0
+        self.isjumper = False
+        self.life = 100
+        self.reward = 0
+        self.active = True
+    def jump(self, jumper_value):
+        if self.isjumper:
+            self.down_gravity = jumper_value
+            self.isjumper = False
+    def fall(self, gravity):
+        self.down_gravity += gravity
+        self.rect.y += self.down_gravity
+    def reset(self, x, y):
+        self.rect.x, self.rect.y = x, y
+        self.down_gravity = 0
+        self.isjumper = False
+        self.life = 100
+        self.reward = 0
+        self.active = True
+    def check_collision(self, other_rect):
+        return self.rect.colliderect(other_rect)
