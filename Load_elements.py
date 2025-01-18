defee0e631da28b710e8ee2cbe65c74a446aaadc
matias <mat20can@gmail.com>
2025-01-18 17:41:40 -0300

    version 10.0

--- Load_elements.py
+++ Load_elements.py
@@ -76,7 +76,8 @@ class load_elements():
         self.config_sounds={"sound_menu":True,"sound_game":True,
                             "sound_jump":True,"game_over":True,
                             "sound_damage":True,"sound_potion":True,"sound_shield":True}
-        self.config_keys={"up1":K_SPACE,"name_up1":"Space","up2":K_w,"name_up2":"W","left":K_a,"right":K_d}
+        self.config_keys={"up1":K_SPACE,"name_up1":"Space","up2":K_w,"name_up2":"W",
+                        "left":K_a,"nane_left":"A","right":K_d,"name_right":"D"}
         self.config_visuals={"player":"flyghost.png","floor":"suelo1.png",
                             "meteorite":"meteorito.png","potion":"pocion1.png","shield":"shield1.png"}
     def save_config(self):
