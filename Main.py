9f750853ecd591f1548878a2557280eb68b4f98a
matias <mat20can@gmail.com>
2025-02-18 16:16:34 -0300

    version 10.0

--- Main.py
+++ Main.py
@@ -8,8 +8,8 @@ if __name__=="__main__":
         match game.mode_game:
             case {"Training AI": True}:
                 best_model = genetic_algorithm(game, input_size=len(game.get_state()), output_size=3,generations=game.config_AI["generation_value"], population_size=game.config_AI["population_value"], num_trials=game.config_AI["try_for_ai"])
-                game.model = best_model
-                if game.config_AI["model_save"]:save_model(game.model, torch.optim.Adam(game.model.parameters(), lr=0.001),game.model_path)
+                game.models[0] = best_model
+                if game.config_AI["model_save"]:save_model(game.models[0], torch.optim.Adam(game.models[0].parameters(), lr=0.001),game.model_path)
             case {"Player": True} | {"AI": True}:game.run_with_models()
         if game.exit:break
 pygame.quit(),sys.exit()
