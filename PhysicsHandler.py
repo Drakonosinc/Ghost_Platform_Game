1e54ff37db55f8e33e93bfabdea81ca5e3eab290
matias <mat20can@gmail.com>
2025-02-26 16:24:03 -0300

    version 10.0

--- PhysicsHandler.py
+++ PhysicsHandler.py
@@ -2,11 +2,6 @@ class PhysicsHandler:
     def __init__(self, gravity=0.25, jump_force=-12):
         self.gravity = gravity
         self.jump_force = jump_force
-
     def apply_gravity(self, player):
         player.dy += self.gravity
         player.rect.y += player.dy
-
-    def handle_jump(self, player,sound=None):
-        player.dy = self.jump_force
-        if sound!=None:sound.play()
