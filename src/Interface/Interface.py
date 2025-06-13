679ec5c13995900080d0a02d45351b3a4b1c3d90
matias <mat20can@gmail.com>
2025-06-13 16:53:38 -0300

    version 10.0

--- src/Interface/Interface.py
+++ src/Interface/Interface.py
@@ -41,11 +41,7 @@ class interface(load_elements,BaseMenu):
                 if self.main==4:self.buttons_options_menu(event)
     def buttons_repetitive(self,event):
         if (self.main==1 or self.main==3) and event.ui_element == self.back_button:self.change_mains(0,command=self.type_game,command2=lambda:[self.on_off_sound(x,y,False) for x,y in zip([self.sound_menu,self.sound_game],["sound_menu","sound_game"])],run=True)
-        if (self.main!=2 and self.main!=4) and event.ui_element == self.option_button:self.change_mains(4)
-        if (self.main!=2 and self.main!=4) and event.ui_element == self.exit_button:self.change_mains(command=self.close_game,sound=self.sound_exit)
         if (self.main!=0 and self.main!=4) and event.ui_element == self.reset_button:self.change_mains(-1,command=self.reset)
-    def buttons_main_menu(self,event):
-        if event.ui_element == self.play_button:self.change_mains(2)
     def buttons_options_menu(self,event):
         if event.ui_element == self.visuals_button:self.change_mains(5)
         if event.ui_element == self.sounds_button:self.change_mains(7)
--- src/Interface/Menus/Main_Menu.py
+++ src/Interface/Menus/Main_Menu.py
@@ -6,9 +6,9 @@ class MainMenu(BaseMenu):
         super().__init__(interface)
         self.buttons = {}
     def setup_buttons(self):
-        self.buttons['play'] = pygame_gui.elements.UIButton(relative_rect=Rect(10, 100, 100, 50),text="Play",manager=self.interface.manager)
-        self.buttons['quit'] = pygame_gui.elements.UIButton(relative_rect=Rect(10, 200, 100, 50),text='Exit',manager=self.manager)
-        self.buttons['options'] = pygame_gui.elements.UIButton(relative_rect=Rect(10, 150, 100, 50),text='Option',manager=self.interface.manager)
+        self.buttons['play'] = pygame_gui.elements.UIButton(relative_rect=Rect(10, 100, 100, 50),text="Play",manager=self.interface.manager,command=lambda: self.change_mains(2))
+        self.buttons['quit'] = pygame_gui.elements.UIButton(relative_rect=Rect(10, 200, 100, 50),text='Exit',manager=self.interface.manager,command=lambda: self.change_mains(command=self.close_game,sound=self.sound_exit))
+        self.buttons['options'] = pygame_gui.elements.UIButton(relative_rect=Rect(10, 150, 100, 50),text='Option',manager=self.interface.manager,command=lambda: self.change_mains(4))
     def render(self):
         self.screen.fill(self.interface.BLACK)
         self.screen.blit(self.interface.font3.render("Ghost Platform",True,self.interface.WHITE),(3,10))
