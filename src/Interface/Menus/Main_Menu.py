571d8103106283d10a6f509da0140d62534aec03
matias <mat20can@gmail.com>
2025-06-13 16:36:16 -0300

    version 10.0

--- src/Interface/Menus/Main_Menu.py
+++ src/Interface/Menus/Main_Menu.py
@@ -1,9 +1,12 @@
 from .Base_Menu import BaseMenu
+from pygame import *
+import pygame_gui
 class MainMenu(BaseMenu):
     def __init__(self, interface):
         super().__init__(interface)
         self.buttons = {}
-    def setup_buttons(self):pass
+    def setup_buttons(self):
+        self.buttons['play'] = pygame_gui.elements.UIButton(relative_rect=Rect(10, 100, 100, 50),text="Play",manager=self.manager)
     def render(self):
         self.screen.fill(self.interface.BLACK)
         self.screen.blit(self.interface.font3.render("Ghost Platform",True,self.interface.WHITE),(3,10))
