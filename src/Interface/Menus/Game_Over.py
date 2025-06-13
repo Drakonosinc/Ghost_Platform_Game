8e023cde80ac9f9d42b9e9a0bb293f75466ce486
matias <mat20can@gmail.com>
2025-06-13 17:00:24 -0300

    version 10.0

--- src/Interface/Menus/Game_Over.py
+++ src/Interface/Menus/Game_Over.py
@@ -7,7 +7,8 @@ class GameOver(BaseMenu):
         self.buttons = {}
     def setup_buttons(self):
         self.buttons['main'] = pygame_gui.elements.UIButton(relative_rect=Rect(10, 150, 150, 50),text='Exit The Menu',manager=self.interface.manager)
-        self.buttons['reset'] = pygame_gui.elements.UIButton(relative_rect=Rect(10, 100, 150, 50),text="Press R to Restart",manager=self.manager)
+        self.buttons['reset'] = pygame_gui.elements.UIButton(relative_rect=Rect(10, 100, 150, 50),text="Press R to Restart",manager=self.interface.manager)
     def render(self):
         self.filt(self.WIDTH,self.HEIGHT,150,self.interface.RED)
         self.screen.blit(self.interface.font4.render("Game Over", True, self.interface.BLACK),(3,10))
+        self.interface.active_buttons = [button for button in self.buttons.values()]
