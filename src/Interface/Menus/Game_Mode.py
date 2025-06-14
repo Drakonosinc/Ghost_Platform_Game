3100569ffda7986cbfe9b11227e8298a5ad62fa5
matias <mat20can@gmail.com>
2025-06-14 16:27:35 -0300

    version 10.0

--- src/Interface/Menus/Game_Mode.py
+++ src/Interface/Menus/Game_Mode.py
@@ -7,5 +7,6 @@ class GameMode(BaseMenu):
         self.buttons = {}
     def setup_buttons(self):
         self.buttons['training_ai'] = pygame_gui.elements.UIButton(relative_rect=Rect(10, 100, 100, 50),text='Training AI',manager=self.interface.manager,command=lambda:self.change_mains(8))
+        self.buttons['player'] = pygame_gui.elements.UIButton(relative_rect=Rect(10, 150, 100, 50),text='Player',manager=self.manager,command=lambda:self.change_mains(-1,command=lambda:self.type_game(mode_two=True),run=True,command2=lambda:self.more_options([self.population,lambda:self.on_off_sound(self.sound_menu,"sound_menu",False,game=True)])))
         
     def render(self):pass
