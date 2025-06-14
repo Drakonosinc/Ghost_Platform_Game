24b5e7bdcf46a297b1c165b5838a88a7d667a05e
matias <mat20can@gmail.com>
2025-06-14 16:42:13 -0300

    version 10.0

--- src/Interface/Menus/Pause.py
+++ src/Interface/Menus/Pause.py
@@ -1,9 +1,13 @@
 from .Base_Menu import BaseMenu
+from pygame import Rect
+import pygame_gui
 class Pause(BaseMenu):
     def __init__(self, interface):
         super().__init__(interface)
         self.buttons = {}
-    def setup_buttons(self):pass
+    def setup_buttons(self):
+        self.buttons['main'] = pygame_gui.elements.UIButton(relative_rect=Rect(10, 150, 150, 50),text='Exit The Menu',manager=self.interface.manager,command=lambda:self.change_mains(0,command=self.type_game,command2=lambda:[self.on_off_sound(x,y,False) for x,y in zip([self.sound_menu,self.sound_game],["sound_menu","sound_game"])],run=True))
+        
     def render(self):
         self.filt(self.WIDTH,self.HEIGHT,150,self.interface.GRAY)
         self.screen.blit(self.interface.font3.render("Pause", True, "White"),(3,10))
