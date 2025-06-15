0c7c783da05bdcdf2efae27a38f62562d9fbd25c
matias <mat20can@gmail.com>
2025-06-15 18:05:28 -0300

    version 10.0

--- src/Interface/Menus/Visuals_Menu.py
+++ src/Interface/Menus/Visuals_Menu.py
@@ -5,3 +5,7 @@ class VisualsMenu(BaseMenu):
     def __init__(self, interface):
         super().__init__(interface)
         self.buttons = {}
+    def setup_buttons(self):pass
+    def render(self):
+        self.setup_buttons()
+        self.interface.active_buttons = [button for button in self.buttons.values()]
