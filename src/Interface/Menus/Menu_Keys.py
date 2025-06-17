07da5e127b05bc75fac919580e7b5d242966720c
matias <mat20can@gmail.com>
2025-06-17 16:16:54 -0300

    version 10.0

--- src/Interface/Menus/Menu_Keys.py
+++ src/Interface/Menus/Menu_Keys.py
@@ -7,4 +7,7 @@ class KeysMenu(BaseMenu):
         self.key = None
         self.key_name = None
         self.button_key = None
-    
+    def setup_buttons(self):pass
+    def render(self):
+        self.setup_buttons()
+        self.interface.active_buttons = [button for button in self.buttons.values()]
