6f433acb0e220131f1b06044c797415dee86cc18
matias <mat20can@gmail.com>
2025-06-18 17:00:52 -0300

    version 10.0

---
+++ src/Interface/Menus/Menu_Sounds.py
@@ -0,0 +1,7 @@
+from pygame import Rect
+from .Base_Menu import BaseMenu
+import pygame_gui
+class KeysMenu(BaseMenu):
+    def __init__(self, interface):
+        super().__init__(interface)
+        self.buttons = {}
