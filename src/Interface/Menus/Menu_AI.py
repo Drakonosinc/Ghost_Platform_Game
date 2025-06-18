2576739659801514b05c9f46f553e4dbd801e9c8
matias <mat20can@gmail.com>
2025-06-18 17:02:31 -0300

    version 10.0

--- src/Interface/Menus/Menu_AI.py
+++ src/Interface/Menus/Menu_AI.py
@@ -6,3 +6,6 @@ class KeysMenu(BaseMenu):
         super().__init__(interface)
         self.buttons = {}
     def setup_buttons(self):pass
+    def render(self):
+        self.setup_buttons()
+        self.interface.active_buttons = [button for button in self.buttons.values()]
